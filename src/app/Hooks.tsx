daf66cf1f2bf725d3d3ac7a5c6af39c4a64c7bbc
Hrishikesh Das <hdrishi@gmail.com>
2025-09-02 14:13:57 +0530

    base commit

--- src/app/Hooks.tsx
+++ src/app/Hooks.tsx
@@ -1,20 +1,42 @@
 "use client";
 
 import { useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 const Hooks = () => {
   const [count, setCount] = useState(0);
+  const [txtInput, setTxtInput] = useState("");
+
+  const handleTextInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setTxtInput(event.target.value);
+  };
 
   return (
     <Container className="mt-5 text-center">
-      <h1>Count: {count}</h1>
+      <Row className="mt-4">
+        <Col>
+          <Form.Control
+            type="text"
+            placeholder="Kindly enter your name"
+            value={txtInput}
+            onChange={handleTextInputChange}
+          />
+        </Col>
+      </Row>
       <Row>
         <Col>
+          <h1>Count: {count}</h1>
           <Button
             variant="outline-success"
             className="mx-2"
-            onClick={() => setCount((count ?? 0) + 1)}
+            onClick={() => {
+              setCount((x) => {
+                debugger;
+                return (count ?? 0) + 1;
+              });
+            }}
             disabled={count >= 10}
           >
             Increment
@@ -28,6 +50,13 @@ const Hooks = () => {
             Decrement
           </Button>
         </Col>
+      </Row>
+      <Row className="mt-4">
+        <input
+          type="text"
+          value={count == 0 ? "" : `${txtInput}, you have entered ${count}`}
+          readOnly
+        />
       </Row>
     </Container>
   );
