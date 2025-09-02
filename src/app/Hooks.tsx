789d03487bf0a7f661ca8684cb9ba1384d195c17
Hrishikesh Das <hdrishi@gmail.com>
2025-09-02 10:42:40 +0530

    base commit

--- src/app/Hooks.tsx
+++ src/app/Hooks.tsx
@@ -5,6 +5,7 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Hooks = () => {
   const [count, setCount] = useState(0);
+
   return (
     <Container className="mt-5 text-center">
       <h1>Count: {count}</h1>
@@ -13,14 +14,16 @@ const Hooks = () => {
           <Button
             variant="outline-success"
             className="mx-2"
-            onClick={() => setCount(count + 1)}
+            onClick={() => setCount((count ?? 0) + 1)}
+            disabled={count >= 10}
           >
             Increment
           </Button>
           <Button
             variant="outline-danger"
             className="mx-2"
-            onClick={() => setCount(count - 1)}
+            onClick={() => setCount((count ?? 0) - 1)}
+            disabled={count <= 0}
           >
             Decrement
           </Button>
